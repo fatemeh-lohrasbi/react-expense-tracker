a96c54803f9ba69d72a232d5bf7659903e9d9181
fatemeh lohrasbi <lohrasbi.developer@gmail.com>
2023-02-10 01:40:04 +0330

    ExpenseDate and AllExpenseItem components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import AllExpenseItem from "./components/AllExpenseItem";
 
 function App() {
-  // const expenseDate = new Date();
-  // const expenseAmount = 'book';
   const expenses = [
     {
       id: 'e1',
@@ -27,11 +25,8 @@ function App() {
 
   return (
     <div className="App">
-     <h1>start expense tracker</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <h1>start expense tracker</h1>
+      <AllExpenseItem items={expenses}  />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
-import './ExpenseItem.css'
- 
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(props) {   
-        return (
+
+function ExpenseItem(props) {
+    return (
         <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>     
-            <div>{props.amount}</div>
-
+            <ExpenseDate date = {props.date} />
             <div className='expense-item__description'>
-                    <h2>{props.title}</h2>
-                <div className='expense-item__price'>$294.67</div>
-            </div>            
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
         </div>
     )
 }
