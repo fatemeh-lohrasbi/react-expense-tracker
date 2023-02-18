b1ef6930176d53dc6bc969b0aa6e5c28f85efac8
fatemeh lohrasbi <lohrasbi.developer@gmail.com>
2023-02-18 22:34:15 +0330

    ExpenseForm added inside of NewExpense component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import AllExpenseItem from "./components/Expenses/AllExpenseItem";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -25,7 +26,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <AllExpenseItem items={expenses} />
     </div>
   );
