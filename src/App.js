7376252a99ba4a737af9f1728a056ca6db08b680
fatemeh lohrasbi <lohrasbi.developer@gmail.com>
2023-03-28 01:36:17 +0430

    change color of some elements and add new initial data

--- src/App.js
+++ src/App.js
@@ -6,26 +6,51 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const initial_expenses = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "Book",
+    amount: 713.33,
+    date: new Date(2023, 1, 14),
   },
   {
-    id: "e2", title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12)
+    id: "e2", 
+    title: "Gym",
+    amount: 950.40,
+    date: new Date(2021, 6, 12)
   },
   {
     id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    title: "Insurance",
+    amount: 380.28,
+    date: new Date(2021, 8, 28),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
+    title: "Clothing",
+    amount: 490,
+    date: new Date(2019, 9, 11),
+  },
+  {
+    id: "e5",
+    title: "Food",
+    amount: 210.38,
+    date: new Date(2020, 2, 17),
+  },
+  {
+    id: "e6",
+    title: "Home repairs",
+    amount: 200.54,
+    date: new Date(2020, 9, 23),
+  },
+  {
+    id: "e7",
+    title: "fuel",
+    amount: 1000,
+    date: new Date(2019, 12, 5),
+  },
+  {
+    id: "e7",
+    title: "Internet",
+    amount: 837.41,
+    date: new Date(2023, 2, 6),
   },
 ];
 
